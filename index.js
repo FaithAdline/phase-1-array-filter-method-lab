09c1e8a69759e73be3f6e28c384e9306e5cf2c46
FaithAdline <adlinefaith@gmail.com>
2023-09-27 20:11:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, target) {
+    return drivers.filter(driver => driver.toLowerCase() === target.toLowerCase());
+}
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  function matchName(drivers, target) {
+    return drivers.filter(driver => driver.name.toLowerCase() === target.toLowerCase());
+  }
+   
+  // Code your solution here
